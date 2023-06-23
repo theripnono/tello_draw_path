ff0d55b03af63b893a58fe681e563fa14097c42e
Theripnono <theripnono@gmail.com>
2023-06-23 10:06:11 +0200

    #Rebuild the program

--- main.py
+++ main.py
@@ -1,130 +1,145 @@
-import pygame, math
-from pygame.math import Vector2
+"""
+The initial code is based on the RobotAndCode YT video.
+Big thanks to him to show the bases of the path planning GUI.
+"""
+
+import pygame, math, json
 
 # Initialize Pygame
 pygame.init()
 
-# Set the width and height of the canvas
-width, height = 300, 300
+# Set the dimensions of the window
+width = 720
+height = 720
 
-# Create the canvas surface
-canvas = pygame.display.set_mode((width, height))
+# Colors Palette
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-# Fill the canvas with white color
-canvas.fill((255, 255, 255))
+# Create the window
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Drone Tracking")
+window.fill(white)
 
-# Variables to track the drawing state
+
+"""
+# RESCALE MAP SIZE #
+As an example I will assume that 20 pixels of the image is equal to 500 cm
+20px = 500cm --> 500 / 20 = 25
+"""
+map_size = 25
+
+path = []
+index = 0
 drawing = False
-start_pos = None
-end_pos = None
-path = []  # List to store the drawn path
-dots = []  # List to store the dots representing the path
 
 
-def draw_path(canvas:classmethod, path:list, dots:list, drawing:bool, start_pos:bool)->None:
+class Background(pygame.sprite.Sprite):
 
-    # Draw the path
-    if len(path) > 1:
-        pygame.draw.lines(canvas, (0, 0, 0), False, path, 2)
-
-    # Draw the dots
-    for dot in dots:
-        pygame.draw.circle(canvas, (255, 0, 0), (int(dot.x), int(dot.y)), 3)
-
-    # Draw the line while drawing is True
-    if drawing and start_pos is not None:
-        current_pos = Vector2(pygame.mouse.get_pos())
-        pygame.draw.line(canvas, (0, 0, 0), path[-1], current_pos, 2)
+    def __init__(self, image, location, scale):
+        pygame.sprite.Sprite.__init__(self)  # Call superclass
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.rotozoom(self.image, 0, scale)  # transform the image
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
 
-    # Draw the line while drawing is True Calculate and display the angle
-        # TODO CALCULATE IN REAL TIME ANGLES
-        if len(path) >= 2:
-            angle = calculate_angle(path[-2], path[-1])
-            rad_text = f"Degree: {angle:.2f}"
-            font = pygame.font.Font(None, 20)
-            text = font.render(rad_text, True, (0, 0, 0))
-            canvas.blit(text, (current_pos.x + 10, current_pos.y + 10))
+def get_distance(p0: tuple, p1: tuple) -> int:
+    """
+    Get Distance between 2 points
+    :param p0: First tuple position
+    :param p1: Second tuple position
+    :return: It returns the distance between these 2 points
+    """
+    x = abs(p0[0] - p1[0])
+    y = abs(p0[1] - p1[1])
+
+    # distance in pixels and cm
+    dist_px = int(math.hypot(x, y))
+    dist_cm = int(dist_px * map_size)  # This value can change depends on or your specific requirements
+
+    return dist_px, dist_cm
 
 
-def calculate_angle(start_pos:list, end_pos:list)->float:
-    direction = end_pos - start_pos
-    angle = math.degrees(math.atan2(direction.y, direction.x))
+def get_angle(p0: tuple, p1: tuple, p_ref: tuple) -> float:
+    """
+    Get angle between two lines respective to p_ref
+    :param p0: First tuple position
+    :param p1: Second tuple position
+    :param p_ref: The referenced created angle between these two points
+    :return: It returns the angle
+    """
+    dx1 = p1[0] - p_ref[0]
+    dy1 = p1[1] - p_ref[1]
+    dx0 = p0[0] - p_ref[0]
+    dy0 = p0[1] - p_ref[1]
+
+    angle1 = math.atan2(dy1, dx1)
+    angle0 = math.atan2(dy0, dx0)
+
+    angle = int(math.degrees(angle1 - angle0))
+
     return angle
 
-def calculate_distance(start_pos, end_pos):
 
-    direction = end_pos - start_pos
-    distance = direction.length()
-    return distance
-
-def store_segments(path):
-
-    segments = {}
-    for i in range(len(path) - 1):
-        start_pos = path[i]
-        end_pos = path[i + 1]
-        direction = end_pos - start_pos
-        angle = calculate_angle(start_pos, end_pos)
-        distance = calculate_distance(start_pos, end_pos)
-        segments[f"Segment {i+1}"] = {
-            "coordinates": {
-                "start_pos": tuple(start_pos),
-                "end_pos": tuple(end_pos)
-            },
-            "vector_results": tuple(direction),
-            "degrees": int(angle),
-            "distance": int(distance)
-        }
-    return segments
-
-
-show_segments = False
+# Load Image
+bground = Background('image.png', [0, 0], 1.2)  # The given image can be rescaled change last parameter
+window.blit(bground.image, bground.rect)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_h:
-                show_segments = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_h:
-                show_segments = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 3:  # Right mouse button
-                drawing = False
-                right_click_pressed = False
-            elif event.button == 1:  # Left mouse button
-                if not drawing:
-                    drawing = True
-                    start_pos = Vector2(event.pos)
-                    path.append(start_pos)
-                    if start_pos is not None:
-                        dots.append(start_pos)
+            drawing = True
+            pos = pygame.mouse.get_pos()
+            path.append(pos)
+        elif event.type == pygame.MOUSEBUTTONUP:
+            drawing = False
 
-                else:
-                    end_pos = Vector2(event.pos)
-                    path.append(end_pos)
-                    dots.append(end_pos)
-                    angle = calculate_angle(path[-2], path[-1])
-                    #print(f"star_pos: {start_pos}")
-                    #print(f"end_pos: {end_pos}")
-                    #print(f"Cosine: {angle}")
-    if show_segments:
-        tracking_path = store_segments(path)
-        print(tracking_path)
+            if index > 0:
+                pygame.draw.line(window, black, path[index - 1], pos, 2)
+            index += 1
 
-    # Clear the canvas
-    canvas.fill((255, 255, 255))
+    pygame.display.update()
 
-    # Call the function to draw the path, dots, and line
-    draw_path(canvas, path, dots, drawing, start_pos)
+#Append first p_ref
+path.insert(0, (path[0][0], path[0][1] - 10)) #Dummy pos_ref
 
-    # Update the display
-    pygame.display.flip()
+# Compute the waypoints
+path_dist_cm = []
+path_dist_px = []
+path_angle = []
 
-# Quit Pygame
-pygame.quit()
-print(tracking_path)
+for index in range(len(path)):
+    #skip the first and second index
+    if index > 1:
+        dist_px, dist_cm = get_distance(path[index - 1], path[index])
+        path_dist_cm.append(dist_cm)
+        path_dist_px.append(dist_px)
+
+    # skip the first and last index
+    if index > 0 and index < (len(path) - 1):
+        angle = get_angle(path[index - 1], path[index + 1], path[index])
+        path_angle.append(angle)
+
+
+# Generate JSON Waypoints
+
+waypoints = []
+
+for index in range(len(path_dist_cm)):
+    waypoints.append({
+        "dist_cm": path_dist_cm[index],
+        "dist_px": path_dist_px[index],
+        "dist_angle": path_angle[index],
+    })
+
+f = open('waypoint.json', 'w+')
+path.pop(0)  # We do not want the dummy position
+json.dump({
+    "wp": waypoints,
+    "pos": path
+}, f, indent=4)
+f.close()
