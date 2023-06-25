e7256b4fe145426c6bca01faba0608d5713f954b
Theripnono <theripnono@gmail.com>
2023-06-25 21:45:20 +0200

    #DEVELOPMENT OF DRAWING PATH IS DONE.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pygame, math, json
+import pygame, math, json, cv2
+from threading import Thread
+from djitellopy import Tello
 # Initialize Pygame
 pygame.init()
 
@@ -35,8 +37,10 @@ objects = []
 
 ms = 100
 ns = 60
-ss =  30
+ss = 30
 
+dir = []
+speed = []
 class Background(pygame.sprite.Sprite):
 
     def __init__(self, image, location, scale):
@@ -59,6 +63,7 @@ class Button():
             'normal': '#555555',
             'hover': '#666666',
             'pressed': '#333333',
+            'right_pressed': '#00FF00'
         }
 
         self.buttonSurface = pygame.Surface((self.width, self.height))
@@ -98,8 +103,31 @@ class Button():
             self.buttonRect.height / 2 - self.buttonSurf.get_rect().height / 2
         ])
         window.blit(self.buttonSurface, self.buttonRect)
-def myFunction():
-    print('Button Pressed')
+def button_forward():
+
+    print('Button Forward')
+    dir.append(1)
+
+def button_left():
+    print('Button Left')
+    dir.append(2)
+
+def button_right():
+    print('Button Right')
+    dir.append(3)
+def button_max_speed():
+    print('Button Max_Speed')
+    speed.append(100)
+def button_normal_speed():
+    print('Button Normal_Speed')
+    speed.append(60)
+def button_slow_speed():
+    print('Button Slow_Speed')
+    speed.append(30)
+
+def button_test ():
+    print('test')
+
 
 def get_distance(p0: tuple, p1: tuple) -> int:
     """
@@ -138,12 +166,13 @@ def get_angle(p0: tuple, p1: tuple, p_ref: tuple) -> float:
 
     return angle
 
-customButton = Button(10,  20, 70, 40, 'Forward', myFunction)
-customButton = Button(100, 20, 70, 40, 'Left', myFunction)
-customButton = Button(200, 20, 70, 40, 'Right', myFunction)
-customButton = Button(300, 20, 90, 40, 'Max_Speed', myFunction)
-customButton = Button(420, 20, 110, 40, 'Normal_Speed', myFunction)
-customButton = Button(560, 20, 100, 40, 'Slow_Speed', myFunction)
+customButton = Button(10,  20, 70, 40, 'Forward', button_forward)
+customButton = Button(100, 20, 70, 40, 'Left', button_left)
+customButton = Button(200, 20, 70, 40, 'Right', button_right)
+customButton = Button(300, 20, 90, 40, 'Max_Speed', button_max_speed)
+customButton = Button(420, 20, 110, 40, 'Normal_Speed', button_normal_speed)
+customButton = Button(560, 20, 100, 40, 'Slow_Speed', button_slow_speed)
+
 
 # Load Image
 bground = Background('image.png', [0, 0], 1.2)  # The given image can be rescaled change last parameter
@@ -197,18 +226,29 @@ for index in range(len(path)):
 # Generate JSON Waypoints
 
 waypoints = []
+path_dir = []
+path_speed = []
+
 
 for index in range(len(path_dist_cm)):
     waypoints.append({
+        "dir": dir[index],
         "dist_cm": path_dist_cm[index],
         "dist_px": path_dist_px[index],
-        "dist_angle": path_angle[index],
+        "angle": path_angle[index],
+        "speed": speed[index]
+
     })
 
 f = open('waypoint.json', 'w+')
 path.pop(0)  # We do not want the dummy position
+
 json.dump({
     "wp": waypoints,
     "pos": path
 }, f, indent=4)
 f.close()
+
+"""
+# TELLO #
+"""
